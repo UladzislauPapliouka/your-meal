5aa54329bca49ddc8efe04e7df6beea786b0355f
Uladzislau Papliouka <uladzislau.papliouka@gmail.com>
2024-02-19 19:33:37 +0300

    feat(footer): add number column

--- src/components/PageFooter/index.tsx
+++ src/components/PageFooter/index.tsx
@@ -1,13 +1,19 @@
 import * as classNames from "classnames";
 import styles from "./index.module.scss";
-
+import call from '../../assets/icons/call.svg'
 export const PageFooter = () =>{
     return(
         <footer className={classNames(styles.footer)}>
             <div className={classNames(styles.mainContainer)}>
                 <div>1</div>
                 <div>2</div>
-                <div>3</div>
+                <div>
+                    <span>Номер для заказа</span>
+                    <div className={classNames(styles.phone)}>
+                        <img src={call} alt={'call'}/>
+                        <span>+7(930)833-38-11</span>
+                    </div>
+                </div>
                 <div>4</div>
             </div>
         </footer>
