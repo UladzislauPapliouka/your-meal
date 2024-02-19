7942392a38862df48f977a3d8c3fdc5cd558680e
Uladzislau Papliouka <uladzislau.papliouka@gmail.com>
2024-02-19 19:50:48 +0300

    feat(footer): add first column

--- src/components/PageFooter/index.tsx
+++ src/components/PageFooter/index.tsx
@@ -1,12 +1,26 @@
 import * as classNames from "classnames";
 import styles from "./index.module.scss";
 import call from '../../assets/icons/call.svg'
+import  telegram from '../../assets/icons/telegram.svg'
+import  vk from '../../assets/icons/vk.svg'
+import tempLogo from "../../assets/icons/tempSecondLogo.svg";
+
+const socials = [
+    telegram,
+    vk
+]
 export const PageFooter = () =>{
     return(
         <footer className={classNames(styles.footer)}>
             <div className={classNames(styles.mainContainer)}>
-                <div>1</div>
-                <div>2</div>
+                <div className={classNames(styles.firstBlock)}>
+                    <img src={tempLogo} alt={'logo'}/>
+                    <div>
+                        <span>© YouMeal, 2023</span>
+                        <span>Develop: Uladzislau Papliouka</span>
+                    </div>
+                </div>
+                <div/>
                 <div>
                     <span>Номер для заказа</span>
                     <div className={classNames(styles.phone)}>
@@ -14,7 +28,12 @@ export const PageFooter = () =>{
                         <span>+7(930)833-38-11</span>
                     </div>
                 </div>
-                <div>4</div>
+                <div>
+                    <span>Мы в соцсетях</span>
+                    <div className={classNames(styles.socials)}>
+                        {socials.map(s=><img src={s} alt={'soc'}/>)}
+                    </div>
+                </div>
             </div>
         </footer>
     )
